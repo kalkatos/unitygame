1140e804d05298ffb3e5ee85d54f890ff3bf7c20
Alex Kalkatos <alexcsts@gmail.com>
2024-07-26 01:15:34 -0300

    OnEnable in start caller

--- Event/StartCaller.cs
+++ Event/StartCaller.cs
@@ -6,11 +6,17 @@ namespace Kalkatos.UnityGame
 	public class StartCaller : MonoBehaviour
 	{
 		[SerializeField] public UnityEvent onAwakeEvent;
+		[SerializeField] public UnityEvent onEnableEvent;
 		[SerializeField] public UnityEvent onStartEvent;
 
         private void Awake ()
         {
 			onAwakeEvent?.Invoke();
+        }
+
+        private void OnEnable ()
+        {
+            onEnableEvent?.Invoke();
         }
 
         private void Start ()
