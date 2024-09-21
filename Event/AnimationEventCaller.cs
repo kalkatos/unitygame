8c0d58518282dbbff3d219a95e24f9f38cc2fb86
Alex Kalkatos <alexcsts@gmail.com>
2024-09-21 00:18:28 -0300

    Animation event caller

---
+++ Event/AnimationEventCaller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Kalkatos.UnityGame
+{
+    public class AnimationEventCaller : MonoBehaviour
+	{
+		[SerializeField] private UnityEvent animationEvent;
+
+		public void InvokeEvent ()
+		{
+			animationEvent?.Invoke();
+		}
+	}
+}
